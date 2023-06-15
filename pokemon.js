0eb8bcae2ab0af8391fa7e3dc4ade46d434163ee
SanEliz18 <sandra_1322134030@uptecamac.edu.mx>
2023-06-14 23:18:11 -0500

    Contador de vida

--- pokemon.js
+++ pokemon.js
@@ -1,13 +1,44 @@
-class pokemon{
+class Pokemon{
     nombre = ''
     elemento = ''
-    vida = 0
+    vida1 = 100
+    vida2 = 100
     ataque = ''
-
-    constructor(nombre,elemento,vida,ataque){
+    daño_ataque1 = 20
+    daño_ataque2 = 15
+    daño_ataque3 = 10
+    constructor(nombre,elemento,ataque){
         this.nombre = nombre
         this.elemento = elemento
-        this.vida = vida
         this.ataque = ataque
+    }
+
+    contadorVida1()
+    {
+        if(this.vida1 != 0)
+        this.vida1 = this.vida1 - this.daño_ataque1
+        
+        else
+        alert("GAME OVER")
+    }
+    contadorVida2()
+    {
+        if(this.vida1 != 0){
+                this.vida1 = this.vida1 - this.daño_ataque2
+            if(this.vida1 < 15){
+                this.vida1 = this.vida1 - this.vida1
+            }
+        }
+
+        else
+        alert("GAME OVER")
+    }
+    contadorVida3()
+    {
+        if(this.vida1 != 0)
+        this.vida1 = this.vida1 - this.daño_ataque3
+
+        else
+        alert("GAME OVER")
     }
 }
